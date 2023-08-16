8b8b9eb2dc75982282187e5015c92e06e2e8780c
TevinWang <tevin.wang@warriorlife.net>
2023-08-16 00:55:35 -0700

    add image stuff

--- src/app/api/index.py
+++ src/app/api/index.py
@@ -1,21 +1,24 @@
 from flask import Flask, request, jsonify, Response
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 import argparse
 import io
 import PIL
 import duckdb
 import lancedb
+import lance
 import pyarrow.compute as pc
 from transformers import CLIPModel, CLIPProcessor, CLIPTokenizerFast
 import gradio as gr
-
-@app.route("/api/python", methods=['GET'])
+@app.route("/api/python", methods=['POST'])
 def get_image_blob():
     # Assuming you have the setup_clip_model, get_table, and embed_func functions
-    # data = request.json
-    setup_clip_model()
+    data = request.get_json()  # Parse incoming JSON data
+    prompt = data.get('prompt', '')  # Get the 'prompt' property from the JSON data
     tbl = get_table()
-    emb = embed_func("car")
+    print(data)
+    emb = embed_func(prompt)
     print(emb)
     result_df = tbl.search(emb).limit(1).to_df()
 
@@ -41,11 +44,19 @@ MODEL_ID = None
 MODEL = None
 TOKENIZER = None
 PROCESSOR = None
+import pyarrow.compute as pc
 
 def get_table():
     db = lancedb.connect("data/lancedb")
-    return db.open_table("images")
-
+    if "diffusiondb" in db.table_names():
+        tbl= db.open_table("diffusiondb")
+    else:
+        # First data processing and full-text-search index
+        data = lance.dataset("rawdata.lance").to_table()
+        # remove null prompts
+        tbl = db.create_table("diffusiondb", data.filter(~pc.field("prompt").is_null()), mode="overwrite")
+        tbl = tbl.create_fts_index(["prompt"])
+    return tbl
 def setup_clip_model():
     global MODEL_ID, MODEL, TOKENIZER, PROCESSOR
     MODEL_ID = "openai/clip-vit-base-patch32"
@@ -57,3 +68,5 @@ def embed_func(query):
     inputs = TOKENIZER([query], padding=True, return_tensors="pt")
     text_features = MODEL.get_text_features(**inputs)
     return text_features.detach().numpy()[0]
+
+setup_clip_model()
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,29 +8,83 @@ import { useState } from "react";
 
 export default function Home() {
   const { data: session } = useSession()
-  const [ prompt, setPrompt ] = useState("");
+  const [image, setImage] = useState();
+  const [prompt, setPrompt] = useState("");
+
+  const promptSubmit = (e) => {
+    // Create the JSON payload with the 'prompt' property
+    const data = {
+      prompt: prompt
+    };
+
+    // Make a POST request using the fetch function
+    fetch("https://2bc8-76-244-43-219.ngrok-free.app/api/python", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.blob())
+      .then(blob => {
+        setImage(blob);
+      })
+      .catch(error => {
+        // Handle any errors that occur during the request
+        console.error("Error:", error);
+      });
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 md:p-12 p-4">
+    <main className="flex min-h-screen flex-col items-center justify-center lg:p-24 md:p-12 p-4">
+      {!image && (
+        <div className="w-1/5 mb-5">
+          <img src="/buildyou.png" fill alt="buildyou logo">
+          </img>
+        </div>
+      )}
       <div className="border border-slate-700 rounded-lg h-auto lg:w-1/3 w-5/6 p-4">
         {session ? (<>
           <div className="flex flex-row justify-center">
-            <p className="text-center align-middle font-semibold h-full mr-3">Signed in as {session.user?.email}</p> 
+            <p className="text-center align-middle font-semibold h-full mr-3">Signed in as {session.user?.email}</p>
             <button onClick={() => signOut()} className="rounded-lg text-xs	bottom-0 bg-zinc-900 hover:bg-zinc-800 transition px-2 py-1">Sign out</button>
           </div>
           <br />
           <h1 className="text-4xl pt-3 font-bold text-center">Your Profile</h1>
-          <div className="flex flex-col items-center justify-center">
+          {!image && (<div className="flex flex-col items-center justify-center">
             <Textarea
-              label="Avatar Prompt" 
+              label="Avatar Prompt"
               variant="flat"
               labelPlacement="inside"
               placeholder="Enter your prompt for your avatar here"
               className="w-full m-3 p-3 rounded-lg text-slate-100 transition"
               onValueChange={setPrompt}
             />
-            <button className="rounded-lg bg-zinc-900 hover:bg-zinc-800 transition p-2">Generate Avatar</button>
-          </div>
+            <button onClick={promptSubmit} className="rounded-lg bg-zinc-900 hover:bg-zinc-800 transition p-2">Generate Avatar</button>
+          </div>)}
+          {image && (
+            <>
+              <div
+                className="absolute inset-0 bg-cover bg-center bg-opacity-75"
+                style={{ backgroundImage: `url(${URL.createObjectURL(image)})`, filter: "brightness(60%)", }}
+              ></div>
+              <div className="absolute bottom-0 left-0 p-10 bg-opacity-50 text-white text-sm">
+                <div className="mb-1 text-5xl font-bold">{prompt}</div>
+                <div>An avatar for {session.user?.email}</div>
+              </div>
+              <a
+                href={URL.createObjectURL(image)}
+                download={`image.png`}
+                className="text-blue-500 bottom-0 p-10 ml-auto mr-auto underline cursor-pointer"
+              >
+                Download
+              </a>
+              <div className="absolute bottom-0 right-0 px-10 py-4 bg-opacity-50 w-1/5 mb-5">
+                <img src="/buildyou.png" fill alt="buildyou logo">
+                </img>
+              </div>
+            </>
+          )}
         </>) : (
           <div className="grid place-content-center">
             <p className="text-center font-semibold">Not signed in</p> <br />
